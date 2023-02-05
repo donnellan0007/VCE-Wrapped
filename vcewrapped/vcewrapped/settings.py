52ccb34717c0d71b635b16154e941abc34905f29
donnellan0007 <donnellan0007@gmail.com>
2023-02-05 13:03:56 +1100

    EC2, etc

--- vcewrapped/vcewrapped/settings.py
+++ vcewrapped/vcewrapped/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-bn^mr1s4ds8pm8-cj4*+30g6g329$r#&(tgxci&b0r*#qp)g8@
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://d4d3-182-239-196-130.au.ngrok.io']
+CSRF_TRUSTED_ORIGINS = ['https://d4d3-182-239-196-130.au.ngrok.io   ']
 
 
 # Application definition
@@ -78,13 +78,26 @@ WSGI_APPLICATION = 'vcewrapped.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
